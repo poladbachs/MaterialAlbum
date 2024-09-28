1a7b6fb63d64a3e16dc620b6b427d33cdb7536f3
Polad <bakhip@usi.ch>
2024-09-28 23:56:13 +0200

    imported needed components like appbar card grid container and icon from MUI 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { Typography } from '@mui/material';
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@mui/material';
+import PhotoCamera from '@mui/icons-material/PhotoCamera';
 
 export default function App() {
     return (
