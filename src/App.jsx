69f7e6eb483c159fa4f15b4bf6e2c9ef3b20a0d1
Polad <bakhip@usi.ch>
2024-09-29 01:05:40 +0200

    adjusted code for MUI 5

--- src/App.jsx
+++ src/App.jsx
@@ -17,14 +17,14 @@ export default function App() {
             <main>
                 <div>
                     <Container maxWidth="sm">
-                        <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
+                        <Typography variant="h2" align="center" color="text.primary" gutterBottom>
                             Photo Album
                         </Typography>
-                        <Typography variant="h5" align="center" color="textSecondary" component="p">
+                        <Typography variant="h5" align="center" color="text.secondary" component="p">
                         A visually engaging photo album application developed using MUI 5 (Material UI), showcasing a seamless user experience for organizing and viewing your favorite memories.
                         </Typography>
                         <div>
-                            <Grid2 container spacing={2} justify="center">
+                            <Grid2 container spacing={2} justifyContent="center">
                                 <Grid2 item>
                                     <Button variant="contained" color="primary">
                                         See my photos
