2566b39474cf744f7c127722591b56a56c7f3531
Polad <bakhip@usi.ch>
2024-09-29 03:01:17 +0200

    created cards array and and mapped each to grid item component

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,20 @@ import { Typography, AppBar, Button, Card, CardActions, CardContent, CardMedia, 
 
 import { StyledContainer, StyledIcon, StyledButton, CardGrid, StyledCardMedia, StyledCardContent } from './styles';
 
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
 export default function App() {
     return (
-        <> 
-            <CssBaseline/>
+        <>
+            <CssBaseline />
             <AppBar position='relative'>
                 <Toolbar>
-                    <StyledIcon/>
+                    <StyledIcon />
                     <Typography variant="h6">
                         Photo Album
                     </Typography>
                 </Toolbar>
-            </AppBar> 
+            </AppBar>
 
             <main>
                 <StyledContainer>
@@ -22,7 +24,7 @@ export default function App() {
                             Photo Album
                         </Typography>
                         <Typography variant="h5" align="center" color="text.secondary" component="p">
-                        A visually engaging photo album application developed using MUI 5 (Material UI), showcasing a seamless user experience for organizing and viewing your favorite memories.
+                            A visually engaging photo album application developed using MUI 5 (Material UI), showcasing a seamless user experience for organizing and viewing your favorite memories.
                         </Typography>
                         <div>
                             <Grid2 container spacing={2} justifyContent="center">
@@ -34,7 +36,7 @@ export default function App() {
                                 <Grid2 item>
                                     <StyledButton variant="outlined" color="primary">
                                         Secondary action
-                                    </StyledButton> 
+                                    </StyledButton>
                                 </Grid2>
                             </Grid2>
                         </div>
@@ -42,26 +44,28 @@ export default function App() {
                 </StyledContainer>
                 <CardGrid maxWidth="md">
                     <Grid2 container spacing={4}>
-                        <Grid2 item>
-                            <Card>
-                                <StyledCardMedia
-                                    image="https://dec-energy.ch/images/mountain-pv.jpeg"
-                                    title="Image Title"
-                                />
-                                <StyledCardContent>
-                                    <Typography gutterBottom variant="h5">
-                                        Heading
-                                    </Typography>
-                                    <Typography>
-                                        This is a media card. Use this section to describe the content.
-                                    </Typography>
-                                </StyledCardContent>
-                                <CardActions>
-                                    <Button size="small" color="primary">View</Button>
-                                    <Button size="small" color="primary">Edit</Button>
-                                </CardActions>
-                            </Card>
-                        </Grid2>
+                        {cards.map(() => (
+                            <Grid2 item>
+                                <Card>
+                                    <StyledCardMedia
+                                        image="https://dec-energy.ch/images/mountain-pv.jpeg"
+                                        title="Image Title"
+                                    />
+                                    <StyledCardContent>
+                                        <Typography gutterBottom variant="h5">
+                                            Heading
+                                        </Typography>
+                                        <Typography>
+                                            This is a media card. Use this section to describe the content.
+                                        </Typography>
+                                    </StyledCardContent>
+                                    <CardActions>
+                                        <Button size="small" color="primary">View</Button>
+                                        <Button size="small" color="primary">Edit</Button>
+                                    </CardActions>
+                                </Card>
+                            </Grid2>
+                        ))}
                     </Grid2>
                 </CardGrid>
             </main>
